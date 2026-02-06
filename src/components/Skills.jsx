ef11f7c4ea9271c6b638f31fa99f5ad1fa6cd31d
Owes Shaikh <ozzyshaikh163@gmail.com>
2026-02-06 17:54:20 +0530

    Update skills list

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import './Styles/Skills.css';
-
 // Import your uploaded assets
 
 const Skills = () => {
@@ -8,19 +6,22 @@ const Skills = () => {
     {
       title: "Languages",
       skills: [
-        { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-        { name: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+        { name: "Java", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" },
         { name: "Python", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
-        { name: "Java", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" }
+        { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+        { name: "HTML", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" }               
+        { name: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
       ]
     },
     {
       title: "Frameworks & Tools",
       skills: [
+         { name: "VS Code", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
         { name: "React.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-        { name: "VS Code", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+        { name: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+        { name: "MongoDB", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" },
         { name: "Android Studio", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" },
-         { name: "Figma", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" }
+        { name: "Figma", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" }
 
         
       ]
@@ -30,7 +31,7 @@ const Skills = () => {
       skills: [
          { name: "Quick Learner", icon: "⚡" },
     { name: "Communication", icon: "💬" },
-    { name: "Teamwork", icon: "🤝" },
+    { name: "Teamwork", icon: "🧩" },
     { name: "Problem Solving", icon: "🧠" },
     { name: "Time Management", icon: "⏱️" },
       ]
